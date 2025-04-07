0e367d163ec217505f03dc56f808a4a4485316ed
Kavindu <156315964+KaviDJ02@users.noreply.github.com>
2025-04-07 21:42:22 +0530

    project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module live.kavinduj.theserenitymhtc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens live.kavinduj.theserenitymhtc to javafx.fxml;
+    exports live.kavinduj.theserenitymhtc;
+}
